78c108bfd1d89c972213217c40cb6826a3c3ad05
EvelynWXY <ningyuewushang@163.com>
2022-06-22 22:17:59 +0800

    deploy

---
+++ precache-manifest.85232c3559b37c7a199c459ddbcbe357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6343eb8137fe14b61c6",
+    "url": "/billing-01-website/css/app.a598b8b1.css"
+  },
+  {
+    "revision": "b3fdc05c3f978c5dc67e1ac07eceff59",
+    "url": "/billing-01-website/index.html"
+  },
+  {
+    "revision": "c6343eb8137fe14b61c6",
+    "url": "/billing-01-website/js/app.f1451369.js"
+  },
+  {
+    "revision": "7422356ec4bf4a339ce6",
+    "url": "/billing-01-website/js/chunk-vendors.07b08d6f.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/billing-01-website/manifest.json"
+  },
+  {
+    "revision": "099c15c3b8d8e3359c6e22e197c02f9f",
+    "url": "/billing-01-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/billing-01-website/robots.txt"
+  }
+]);
